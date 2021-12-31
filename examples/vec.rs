267d49724f322a04b0e746a9bff708b84dd98ed9
deception <tsujianii@tuta.io>
2021-12-31 20:49:39 +0800

    show the constness

--- examples/vec.rs
+++ examples/vec.rs
@@ -1,3 +1,5 @@
+#![feature(const_trait_impl)]
+
 use meth::Vec;
 
 fn main() {
@@ -5,13 +7,27 @@ fn main() {
         1_u32, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
     ]);
 
-    println!("{:?}", vec + vec);
-    println!("{:?}", vec / vec);
-    println!("{:?}", vec * vec);
-    println!("{:?}", vec % vec);
-    println!("{:?}", vec - vec);
+    println!("vec + vec = {:?}", vec + vec);
+    println!("vec / vec = {:?}", vec / vec);
+    println!("vec * vec = {:?}", vec * vec);
+    println!("vec % vec = {:?}", vec % vec);
+    println!("vec - vec = {:?}", vec - vec);
 
     let vec = Vec::from_array([1_u32, 2, 3]);
 
-    println!("{:?}", vec + vec);
+    println!("vec + vec = {:?}", vec + vec);
+
+    const TESTS: [Vec<u32, 18>; 5] = {
+        let vec = Vec::from_array([
+            1_u32, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+        ]);
+
+        [vec + vec, vec / vec, vec * vec, vec % vec, vec - vec]
+    };
+
+    println!("vec + vec = {:?}", TESTS[0]);
+    println!("vec / vec = {:?}", TESTS[1]);
+    println!("vec * vec = {:?}", TESTS[2]);
+    println!("vec % vec = {:?}", TESTS[3]);
+    println!("vec - vec = {:?}", TESTS[4]);
 }
